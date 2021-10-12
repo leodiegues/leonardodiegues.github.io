73b9bfc82570892b4518d4a607004fcd9ae45838
Leonardo Diegues <dscleo@gmail.com>
2021-10-12 00:08:17 -0300

    Deploy Next.js to gh-pages

---
+++ out/_next/static/QDwZbZxLQ8Oa6oI0ArYJw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fcc151b5d25b3e295eff3.css","static\u002Fchunks\u002Fpages\u002Findex-f08df01579b6ef269d3f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-946fe01d054a97e8b849.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
